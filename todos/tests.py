cdfdd31c424b038b40f52187a1d4ec098e06db03
José Arias <j.ariascanto@gmail.com>
2022-06-09 23:09:48 -0500

    added Todo model tests

--- todos/tests.py
+++ todos/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Todo
+
+
+class TodoModelTest(TestCase):
+
+    def setUp(self):
+        self.todo = Todo.objects.create(
+            title='Test todo',
+            body='This is a test'
+        )
+
+    def test_todo_content(self):
+        todo = Todo.objects.get(id=1)
+        self.assertEqual(todo.title, 'Test todo')
+        self.assertEqual(todo.body, 'This is a test')
