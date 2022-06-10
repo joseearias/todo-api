73b90fd8dc63fd141de0569a1854e31d93ce624b
José Arias <j.ariascanto@gmail.com>
2022-06-09 23:30:11 -0500

    added created_at field to todo model

--- todos/serializers.py
+++ todos/serializers.py
@@ -7,4 +7,4 @@ class TodoSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Todo
-        fields = ('id', 'title', 'body',)
+        fields = ('id', 'title', 'body', 'created_at')
