0717388d45e0aec0eabeee1ffcb991ba0b0e8aaf
José Arias <j.ariascanto@gmail.com>
2022-06-09 22:50:35 -0500

    fixed settings.py bug

--- config/settings.py
+++ config/settings.py
@@ -51,7 +51,7 @@ REST_FRAMEWORK = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.common.CommonMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
