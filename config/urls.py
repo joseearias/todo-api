d5d0fdcc0d07ac5daa749263d5d302e6848e3c95
José Arias <j.ariascanto@gmail.com>
2022-06-14 20:15:29 -0500

    changed ListTodo view

--- config/urls.py
+++ config/urls.py
@@ -3,6 +3,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('tasks/', include('todos.urls')),
+    path('todos/', include('todos.urls')),
     path('api-auth/', include('rest_framework.urls')),
 ]
--- todos/views.py
+++ todos/views.py
@@ -1,14 +1,18 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 
 from .models import Todo
 from .serializers import TodoSerializer
 
 
 class ListTodo(generics.ListCreateAPIView):
-    queryset = Todo.objects.all()
+    permission_classes = (permissions.IsAuthenticated,)
     serializer_class = TodoSerializer
+
+    def get_queryset(self):
+        return Todo.objects.all().filter(author=self.request.user)
 
 
 class DetailTodo(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (permissions.IsAuthenticated,)
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
