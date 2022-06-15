116e4fbd9098a402c6e7ad4ffbbdcfb728e67d81
José Arias <j.ariascanto@gmail.com>
2022-06-14 19:58:08 -0500

    changed todos url path

--- config/urls.py
+++ config/urls.py
@@ -3,6 +3,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('todos.urls')),
+    path('tasks/', include('todos.urls')),
     path('api-auth/', include('rest_framework.urls')),
 ]
