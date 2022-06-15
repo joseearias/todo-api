d633d822f528eeb84e7b81bbc2e6bd81f6ce9db7
José Arias <j.ariascanto@gmail.com>
2022-06-14 19:28:56 -0500

    removed Date field

--- todos/serializers.py
+++ todos/serializers.py
@@ -7,4 +7,4 @@ class TodoSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Todo
-        fields = ('id', 'title', 'body', 'created_at')
+        fields = ('id', 'title', 'body')
